7edc27787a74d59b9d3c59106a87414831bb80ff
mobutuseseku <irfan.bosnjak86@gmail.com>
2017-10-20 15:23:12 +0200

    refactoring-tests: Setups a database cleaner.

--- features/support/env.rb
+++ features/support/env.rb
@@ -57,13 +57,21 @@ end
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 # Cucumber::Rails::Database.javascript_strategy = :truncation
 
-# Capybara.register_driver :chrome do |app|
-#   Capybara::Selenium::Driver.new(app, :browser => :chrome)
-# end
 
 # Capybara.javascript_driver = :chrome
 
 Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, :browser => :firefox)
+end
+
+DatabaseCleaner.clean_with :truncation # clean once to ensure clean slate
+DatabaseCleaner.strategy = :truncation
+
+Before('@no-txn') do
+  DatabaseCleaner.start
+end
+
+After('@no-txn') do
+  DatabaseCleaner.clean
 end
 
