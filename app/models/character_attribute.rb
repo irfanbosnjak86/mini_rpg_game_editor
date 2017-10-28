92950fe78386730824f139fc60e8096f02314a7b
mobutuseseku <irfan.bosnjak86@gmail.com>
2017-10-29 01:09:05 +0200

    add-validations-to-attr: Added numerical validations to numerical value of char attr.

--- app/models/character_attribute.rb
+++ app/models/character_attribute.rb
@@ -2,4 +2,5 @@ class CharacterAttribute < ApplicationRecord
   belongs_to :character_type
   mount_uploader :char_attr_avatar, CharAttrAvatarUploader
   validates :name, :numerical_value, :char_attr_avatar, presence: true
+  validates_numericality_of :numerical_value, less_than_or_equal_to: 999, greater_than: 0
 end
